9820c5490ad557226dec3991fe3eb005f206f5d2
choseongmin1997 <55073966+choseongmin1997@users.noreply.github.com>
2021-05-26 23:43:03 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,34 +1,34 @@
-function kakaoShare() {
-  Kakao.Link.sendDefault({
-  objectType: 'feed',
-  content: {
-    title: '디저트 사진',
-    description: '아메리카노, 빵, 케익',
-    imageUrl:
-      'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
-    link: {
-      mobileWebUrl: 'https://developers.kakao.com',
-      androidExecParams: 'test',
-    },
-  },
-  social: {
-    likeCount: 10,
-    commentCount: 20,
-    sharedCount: 30,
-  },
-  buttons: [
-    {
-      title: '웹으로 이동',
+  function kakaoShare() {
+    Kakao.Link.sendDefault({
+    objectType: 'feed',
+    content: {
+      title: '디저트 사진',
+      description: '아메리카노, 빵, 케익',
+      imageUrl:
+        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
       link: {
         mobileWebUrl: 'https://developers.kakao.com',
+        androidExecParams: 'test',
       },
     },
-    {
-      title: '앱으로 이동',
-      link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-      },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
     },
-  ]
-});
-}
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+      {
+        title: '앱으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+    ]
+  });
+  }

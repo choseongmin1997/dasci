a539ee878c87054fc918d7f6d7d1703b2791d3ef
choseongmin1997 <55073966+choseongmin1997@users.noreply.github.com>
2021-05-26 17:05:10 +0900

    논문 파일

---
+++ home/js/share.js
@@ -0,0 +1,35 @@
+function kakaoShare() {
+  Kakao.Link.createDefaultButton({
+    container: '#CONTAINER_ID',
+    objectType: 'feed',
+    content: {
+      title: '디저트 사진',
+      description: '아메리카노, 빵, 케익',
+      imageUrl:
+        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+      link: {
+        mobileWebUrl: 'https://developers.kakao.com',
+        androidExecParams: 'test',
+      },
+    },
+    social: {
+      likeCount: 10,
+      commentCount: 20,
+      sharedCount: 30,
+    },
+    buttons: [
+      {
+        title: '웹으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+      {
+        title: '앱으로 이동',
+        link: {
+          mobileWebUrl: 'https://developers.kakao.com',
+        },
+      },
+    ]
+  });
+}

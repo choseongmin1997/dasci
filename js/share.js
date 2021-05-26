38de41ed50b102bbfb5083ca96e5498c07be73c7
choseongmin1997 <55073966+choseongmin1997@users.noreply.github.com>
2021-05-27 00:03:43 +0900

    Add files via upload

--- js/share.js
+++ js/share.js
@@ -1,34 +1,32 @@
-  function kakaoShare() {
-    Kakao.Link.sendDefault({
-    objectType: 'feed',
-    content: {
-      title: '디저트 사진',
-      description: '아메리카노, 빵, 케익',
-      imageUrl:
-        'http://mud-kage.kakao.co.kr/dn/NTmhS/btqfEUdFAUf/FjKzkZsnoeE4o19klTOVI1/openlink_640x640s.jpg',
+const url = 'https://mytravelerstyle.netlify.app/';
+
+function setShare() {
+  var resultImg = document.querySelector('#resultImg');
+  var resultAlt = resultImg.firstElementChild.alt;
+  const shareTitle = '여행성향 확인하기';
+  const shareDes = infoList[resultAlt].name;
+  const shareImage = url + 'img/image-' + resultAlt + '.jpg';
+  const shareURL = url + 'page/result-' + resultAlt + '.html';
+
+  Kakao.Link.sendDefault({
+  objectType: 'feed',
+  content: {
+    title: shareTitle,
+    description: shareDes,
+    imageUrl: shareImage,
+    link: {
+      mobileWebUrl: shareURL,
+      webUrl: shareURL,
+    },
+  },
+  buttons: [
+    {
+      title: '결과 확인하기',
       link: {
-        mobileWebUrl: 'https://developers.kakao.com',
-        androidExecParams: 'test',
+        mobileWebUrl: shareURL,
+        webUrl: shareURL
       },
-    },
-    social: {
-      likeCount: 10,
-      commentCount: 20,
-      sharedCount: 30,
-    },
-    buttons: [
-      {
-        title: '웹으로 이동',
-        link: {
-          mobileWebUrl: 'https://developers.kakao.com',
-        },
-      },
-      {
-        title: '앱으로 이동',
-        link: {
-          mobileWebUrl: 'https://developers.kakao.com',
-        },
-      },
-    ]
-  });
-  }
+    };
+  ]
+});
+}
